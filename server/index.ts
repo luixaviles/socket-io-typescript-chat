61487c5f0e53f3b8a2b0b16f0a266029dc2b3f6f
luixaviles <luis.avilesz@gmail.com>
2016-11-10 22:42:40 -0400

    Adding event handler into server class

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import * as http from "http";
 import * as socketIo from "socket.io";
 
 class Server {
+    public static readonly PORT = 8080;
     public app: any;
     private server: any;
     private io: any;
@@ -29,7 +30,7 @@ class Server {
     }
 
     private config(): void {
-        this.port = process.env.PORT || 8080;
+        this.port = process.env.PORT || Server.PORT;
     }
 
     private sockets(): void {
@@ -38,15 +39,19 @@ class Server {
 
     private listen(): void {
         this.server.listen(this.port, () => {
-            console.log('Running server');
+            console.log('Running server on port %s', this.port);
         });
 
-        this.io.on('connection', (socket) => {
-            console.log('==> Connected client on port %s.', this.port);
+        this.io.on('connect', (socket) => {
+            console.log('Connected client on port %s.', this.port);
             socket.on('message', (message) => {
-                console.log('[server](message): %s', message);
+                console.log('[server](message): %s', JSON.stringify(message));
                 this.io.emit('message', message);
-            })
+            });
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
         });
     }
 }
