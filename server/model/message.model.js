218898654635c08b4bd846f8f29de05dfd408425
luixaviles <luis.avilesz@gmail.com>
2016-11-10 23:13:42 -0400

    Removing js files

--- server/model/message.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Message = (function () {
-    function Message(from, content) {
-        this.from = from;
-        this.content = content;
-    }
-    return Message;
-}());
-exports.Message = Message;
