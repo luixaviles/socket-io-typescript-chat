21fe7f82a26364318a78736ab553230c44539119
luixaviles <//luix_8505>
2016-11-29 19:19:54 -0400

    Adding main layout of chat room. Using fake data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Message, User } from './';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  messages: Message[];
+
+  ngOnInit(): void {
+    this.initModel();
+  }
+
+  initModel(): void {
+    this.messages = [
+      new Message(new User('luis', 'http://avatar.3sd.me/80'), 'Hi there'),
+      new Message(new User('juan', 'http://avatar.3sd.me/80'), 'How are you'),
+    ];
+  }
+
 }
