71364e560fd798badfd17d3ac7d2d559fd9c2864
luixaviles <luis.avilesz@gmail.com>
2016-11-30 15:46:41 -0400

    Adding socket.io as part of client side project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {Message} from './shared/message.model';
 import {User} from './shared/user.model';
+import {SocketService} from './shared/socket.service';
 
 let AVATAR_URL = 'http://avatar.3sd.me/80';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [SocketService]
 })
 export class AppComponent implements OnInit {
     user: User = new User('random-user', AVATAR_URL);
@@ -15,18 +17,30 @@ export class AppComponent implements OnInit {
         new Message(this.user, 'Hi there'),
         new Message(new User('juan', AVATAR_URL), 'How are you'),
     ];
-    messageString: string;
+    messageContent: string;
+    ioConnection: any;
+
+    constructor(private socketService: SocketService) {
+
+    }
 
     ngOnInit(): void {
         this.initModel();
+        this.initIoConnection();
     }
 
     initModel(): void {
-        this.messageString = '';
+        this.messageContent = '';
+    }
+
+    initIoConnection(): void {
+        this.ioConnection = this.socketService.get().subscribe((message: Message) => {
+            this.messages.push(message)
+        });
     }
 
     sendMessage(): void {
-        this.messages.push(new Message(this.user, this.messageString));
+        this.socketService.send(new Message(this.user, this.messageContent));
         this.initModel();
     }
 }
