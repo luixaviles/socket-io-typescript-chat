5a848cf095c7e65ad75167940a27551298e3e53b
luixaviles <luis.avilesz@gmail.com>
2016-11-30 11:19:36 -0400

    Adding data bindings

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,31 @@
 import {Component, OnInit} from '@angular/core';
 import {Message, User} from './';
 
+let AVATAR_URL = 'http://avatar.3sd.me/80';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    user: User = new User('random-user', 'http://avatar.3sd.me/80');
+    user: User = new User('random-user', AVATAR_URL);
     messages: Message[] = [
         new Message(this.user, 'Hi there'),
-        new Message(new User('juan', 'http://avatar.3sd.me/80'), 'How are you'),
+        new Message(new User('juan', AVATAR_URL), 'How are you'),
     ];
+    messageString: string;
 
     ngOnInit(): void {
         this.initModel();
     }
 
     initModel(): void {
-
+        this.messageString = '';
     }
 
+    sendMessage(): void {
+        this.messages.push(new Message(this.user, this.messageString));
+        this.initModel();
+    }
 }
