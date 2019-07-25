8171a5e7e6ef388dd8c88737a85eb524f9329961
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-24 23:11:45 -0300

    Fixing memory leak in events
    * chat.component.ts:
     * When we use the subscribe, according to the documentation it is advisable make the unsubscribe by subscription on method ngOnDestroy
    
    * socket.service.ts
      * It's more usefull use the operators from reactive to listem events

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef } from '@angular/core';
-import { MatDialog, MatDialogRef, MatList, MatListItem } from '@angular/material';
+import {Component, OnInit, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef, OnDestroy} from '@angular/core';
+import {MatDialog, MatDialogRef, MatList, MatListItem} from '@angular/material';
 
-import { Action } from './shared/model/action';
-import { Event } from './shared/model/event';
-import { Message } from './shared/model/message';
-import { User } from './shared/model/user';
-import { SocketService } from './shared/services/socket.service';
-import { DialogUserComponent } from './dialog-user/dialog-user.component';
-import { DialogUserType } from './dialog-user/dialog-user-type';
-
+import {Action} from './shared/model/action';
+import {Event} from './shared/model/event';
+import {Message} from './shared/model/message';
+import {User} from './shared/model/user';
+import {SocketService} from './shared/services/socket.service';
+import {DialogUserComponent} from './dialog-user/dialog-user.component';
+import {DialogUserType} from './dialog-user/dialog-user-type';
+import {Subscription} from 'rxjs/Subscription';
 
 const AVATAR_URL = 'https://api.adorable.io/avatars/285';
 
@@ -17,13 +17,19 @@ const AVATAR_URL = 'https://api.adorable.io/avatars/285';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, AfterViewInit {
+export class ChatComponent implements OnInit, AfterViewInit, OnDestroy {
+  private subscriptionOnEventConnected: Subscription;
+  private subscriptionOnEventDisconnect: Subscription;
+  private ioConnectionSubscription: Subscription;
+
   action = Action;
+
   user: User;
+
   messages: Message[] = [];
   messageContent: string;
-  ioConnection: any;
   dialogRef: MatDialogRef<DialogUserComponent> | null;
+
   defaultDialogUserParams: any = {
     disableClose: true,
     data: {
@@ -33,13 +39,13 @@ export class ChatComponent implements OnInit, AfterViewInit {
   };
 
   // getting a reference to the overall list, which is the parent container of the list items
-  @ViewChild(MatList, { read: ElementRef }) matList: ElementRef;
-
-  // getting a reference to the items/messages within the list
-  @ViewChildren(MatListItem, { read: ElementRef }) matListItems: QueryList<MatListItem>;
+  @ViewChild(MatList, {read: ElementRef}) matList: ElementRef;
+  // getting a reference to the items/messages$ within the list
+  @ViewChildren(MatListItem, {read: ElementRef}) matListItems: QueryList<MatListItem>;
 
   constructor(private socketService: SocketService,
-    public dialog: MatDialog) { }
+              public dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.initModel();
@@ -50,10 +56,21 @@ export class ChatComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // subscribing to any changes in the list of items / messages
-    this.matListItems.changes.subscribe(elements => {
+    // subscribing to any changes in the list of items / messages$
+    this.matListItems.changes.subscribe(() => {
       this.scrollToBottom();
     });
+  }
+
+
+  ngOnDestroy(): void {
+    [
+      this.subscriptionOnEventConnected,
+      this.subscriptionOnEventDisconnect,
+      this.ioConnectionSubscription
+    ]
+      .filter(subscription => subscription)
+      .forEach(subscription => subscription.unsubscribe())
   }
 
   // auto-scroll fix: inspired by this stack overflow post
@@ -76,24 +93,25 @@ export class ChatComponent implements OnInit, AfterViewInit {
   private initIoConnection(): void {
     this.socketService.initSocket();
 
-    this.ioConnection = this.socketService.onMessage()
+    this.ioConnectionSubscription = this.socketService.onMessage()
       .subscribe((message: Message) => {
         this.messages.push(message);
       });
 
 
-    this.socketService.onEvent(Event.CONNECT)
+    this.subscriptionOnEventConnected = this.socketService
+      .onEvent(Event.CONNECT)
       .subscribe(() => {
         console.log('connected');
       });
 
-    this.socketService.onEvent(Event.DISCONNECT)
+    this.subscriptionOnEventDisconnect = this.socketService.onEvent(Event.DISCONNECT)
       .subscribe(() => {
         console.log('disconnected');
       });
   }
 
-  private getRandomId(): number {
+  getRandomId(): number {
     return Math.floor(Math.random() * (1000000)) + 1;
   }
 
--- client/src/app/chat/shared/services/socket.service.ts
+++ client/src/app/chat/shared/services/socket.service.ts
@@ -1,34 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
-import { Message } from '../model/message';
-import { Event } from '../model/event';
-
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Message} from '../model/message';
+import {Event} from '../model/event';
 import * as socketIo from 'socket.io-client';
+import {fromEvent} from 'rxjs/observable/fromEvent';
 
 const SERVER_URL = 'http://localhost:8080';
 
 @Injectable()
 export class SocketService {
-    private socket;
+  private socket;
 
-    public initSocket(): void {
-        this.socket = socketIo(SERVER_URL);
-    }
+  public initSocket(): void {
+    this.socket = socketIo(SERVER_URL);
+  }
 
-    public send(message: Message): void {
-        this.socket.emit('message', message);
-    }
+  public send(message: Message): void {
+    this.socket.emit('message', message);
+  }
 
-    public onMessage(): Observable<Message> {
-        return new Observable<Message>(observer => {
-            this.socket.on('message', (data: Message) => observer.next(data));
-        });
-    }
+  public onMessage(): Observable<Message> {
+    return fromEvent(this.socket, 'message')
+  }
 
-    public onEvent(event: Event): Observable<any> {
-        return new Observable<Event>(observer => {
-            this.socket.on(event, () => observer.next());
-        });
-    }
+  public onEvent(event: Event): Observable<any> {
+    return fromEvent(this.socket, event);
+  }
 }
